a2c85304c7c956aa079cc26d368441518bddaf62
ongebub <ongebub@gmail.com>
2026-04-14 16:05:52 -0500

    Fix Google profile URL with real Maps link

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,3 +5,4 @@ export const COMPANY_PHONE = "515-953-4000";
 export const COMPANY_EMAIL = "info@TaylorExt.com";
 export const COMPANY_ADDRESS = "1155 Southwold Rd. Des Moines, IA 50320";
 export const COMPANY_WEBSITE = "https://taylorext.com";
+export const GOOGLE_PROFILE_URL = "https://www.google.com/maps/place/Taylor+Exteriors+%26+Construction/@41.5101962,-93.6051509,17z/data=!4m8!3m7!1s0x87eea2e6d19fbe53:0x99d1540d854ce965!8m2!3d41.5101962!4d-93.6051509!9m1!1b1!16s%2Fg%2F11ghr0ks3f?entry=ttu&g_ep=EgoyMDI2MDQxMi4wIKXMDSoASAFQAw%3D%3D"
